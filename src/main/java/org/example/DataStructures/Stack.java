50d8a089776379fc3adf4eddae521e9f278b9f6d
Caleb Wilson <calebwilson706@gmail.com>
2023-03-09 21:06:10 +0000

    First commit

---
+++ src/main/java/org/example/DataStructures/Stack.java
@@ -0,0 +1,38 @@
+package org.example.DataStructures;
+
+public class Stack<T> {
+    private Node<T> top;
+
+    public Stack() {
+        this.top = null;
+    }
+
+    public boolean isEmpty() {
+        return this.top == null;
+    }
+
+    public void push(T value) {
+        Node<T> newNode = new Node<T>(value);
+        newNode.setNext(this.top);
+        this.top = newNode;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+
+        T data = this.top.getValue();
+        this.top = this.top.getNext();
+        return data;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+
+        return this.top.getValue();
+    }
+}
+
