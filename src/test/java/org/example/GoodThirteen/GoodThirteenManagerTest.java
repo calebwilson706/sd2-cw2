3fe4f56f6e2fae67f8137b6bbf242181f055527f
Caleb Wilson <calebwilson706@gmail.com>
2023-03-13 20:10:44 +0000

    Finish testing manager class

--- src/test/java/org/example/GoodThirteen/GoodThirteenManagerTest.java
+++ src/test/java/org/example/GoodThirteen/GoodThirteenManagerTest.java
@@ -1,6 +1,8 @@
 package org.example.GoodThirteen;
 
 import org.example.Deck.Card;
+import org.example.Deck.Rank;
+import org.example.Deck.Suit;
 import org.example.GameStatus;
 import org.example.Utilities.InputService;
 import org.junit.jupiter.api.BeforeEach;
@@ -58,6 +60,56 @@ class GoodThirteenManagerTest {
         verify(history, times(0)).replayHistory();
     }
 
+    @Test
+    public void testPlayUserControlledDoesReplayHistory() throws IOException {
+        // Given the game will end
+        // And the history is not replayed
+        Mockito.when(goodThirteen.getGameStatus())
+                .thenReturn(GameStatus.SUCCESSFUL);
+
+        // When the game is completed and replay is requested
+        Mockito.when(inputService.getYesOrNoAnswer(Mockito.anyString()))
+                .thenReturn(true);
+        target.playUserControlledGame();
+
+        // It should replay the history
+        verify(history, times(1)).replayHistory();
+    }
+
+    @Test
+    public void testPlayUserControlledGamePlayThrough() throws IOException {
+        // Given the game will end after 2 iterations
+        Mockito.when(goodThirteen.getGameStatus())
+                .thenReturn(GameStatus.TO_BE_CONTINUED)
+                .thenReturn(GameStatus.TO_BE_CONTINUED)
+                .thenReturn(GameStatus.FAILED);
+        Card[] move1 = new Card[] { new Card(Rank.KING, Suit.SPADES)};
+        Card[] move2 = new Card[] { new Card(Rank.KING, Suit.HEARTS)};
+        Mockito.when(goodThirteen.getUsersNextMove())
+                .thenReturn(move1)
+                .thenReturn(move2);
+
+        // And the game is carried out
+        target.playUserControlledGame();
+
+        // It should carry out 2 iterations
+        verify(goodThirteen, times(2)).displayActiveCards();
+        verify(goodThirteen, times(2)).displayHint();
+        verify(goodThirteen, times(2)).getUsersNextMove();
+
+        // It should execute the two returned moves
+        verify(goodThirteen, times(1)).executeUsersNextMove(move1);
+        verify(goodThirteen, times(1)).executeUsersNextMove(move2);
+
+        // It should add the two moves to history
+        verify(history, times(1)).enqueue(
+                Mockito.argThat(argument -> argument.move() == move1)
+        );
+        verify(history, times(1)).enqueue(
+                Mockito.argThat(argument -> argument.move() == move2)
+        );
+    }
+
     private void setUpInputService() {
         inputService = Mockito.mock(InputService.class);
         Mockito.when(inputService.getYesOrNoAnswer(Mockito.anyString()))
@@ -67,6 +119,8 @@ class GoodThirteenManagerTest {
     private void setUpHistory() {
         history = Mockito.mock(GoodThirteenHistory.class);
         factory = Mockito.mock(GoodThirteenHistoryFactory.class);
+        Mockito.when(goodThirteen.getActiveCards())
+                        .thenReturn(new Card[] {});
         Mockito.when(factory.create(Mockito.any(Card[].class), Mockito.any(InputService.class)))
                 .thenReturn(history);
     }
